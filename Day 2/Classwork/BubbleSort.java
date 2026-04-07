efc28db374c4f7f568f37e40ce995c9391b12cb9
GenDelta <ankush.dutta.kol@gmail.com>
2026-04-07 10:05:03 +0530

    Day 2 Sorting Questions

---
+++ Day 2/Classwork/BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        // int arr[] = {64,34,25,12,11,90};
+        int arr[] = {11,12,13,14,15};
+        boolean swapped = false;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+                break;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
